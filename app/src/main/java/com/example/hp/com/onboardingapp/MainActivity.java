fad0941c4d9898ac5de1b862b26830aeeaeb3981
mitulp534 <mitulp534@gmail.com>
2018-11-06 00:19:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/com/onboardingapp/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.hp.com.onboardingapp;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+
+
+    private SliderAdapter sliderAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSlideViewPager = (ViewPager)findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout)findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(this);
+
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+    }
+    public void addDotsIndicator(int position){
+        mDots = new TextView[4];
+        mDotLayout.removeAllViews();
+
+        for(int i=0; i<mDots.length;i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+            mDotLayout.addView(mDots[i]);
+        }
+        if(mDots.length>0){
+            mDots[position].setTextColor(getResources().getColor(R.color.colorWhite));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/example/hp/com/onboardingapp/SliderAdapter.java
@@ -0,0 +1,79 @@
+package com.example.hp.com.onboardingapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SliderAdapter extends PagerAdapter {
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context){
+        this.context = context;
+    }
+
+    //arrays for image
+    public int[] slide_images = {
+            R.drawable.slide_welcome,
+            R.drawable.slide_project,
+            R.drawable.slide_skills,
+            R.drawable.slide_professional
+    };
+
+    //for headings
+    public String[] slide_headings = {
+            "Journey till the Professional Developer",
+            "Projects makes developer perfect",
+            "Increase skills by working on projects",
+            "Are you ready to become a professional developer?"
+
+    };
+
+    //for description
+    public String[] slide_description = {
+            "Start your journey from zero development skills to the professional level skills. Increase your logical thinking, measure your progress of programming, Solve the Errors and Get better and better, Everyday until you become the professional developer" +
+                    "Practice makes a man perfect similarly building project makes developer perfect.  " +
+                    "Improve your logical programming skills with competitive questions in the project." +
+                    "Make yourself confident and comfortable for the future projects and your new professional developer life."
+    };
+
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return view == (RelativeLayout) o;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(ViewGroup container,int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view  = layoutInflater.inflate(R.layout.slide_layout,container,false);
+
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_image);
+        TextView slideHeading = (TextView) view.findViewById(R.id.heading_slideScreen1);
+        TextView slideDescription = (TextView) view.findViewById(R.id.description_slideScreen1);
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slide_description[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((RelativeLayout)object);
+    }
+}
